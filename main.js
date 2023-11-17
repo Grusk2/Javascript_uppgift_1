0245292fbafbc6e5ee4aec0fb3a7ead83393dc86
Grusk9 <71376088+Grusk9@users.noreply.github.com>
2023-11-17 16:52:14 +0100

    init comit

---
+++ main.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const restartBtn = document.getElementById('restartBtn');
+    const counterSpan = document.getElementById('count');
+    const items = ['item1', 'item2', 'item3'];
+    const flashlightRadius = 150;
+    let collectedItems = parseInt(localStorage.getItem('collectedItems')) || 0;
+  
+    // Restart button funtion
+    restartBtn.addEventListener('click', function () {
+      clearItems();
+      generateRandomItems();
+      collectedItems = 0;
+      updateCounter();
+    });
+  
+    // This clears the previous items
+    function clearItems() {
+      items.forEach(item => {
+        const itemElement = document.getElementById(item);
+        if (itemElement) {
+          itemElement.remove();
+          localStorage.removeItem(item);
+        }
+      });
+    }
+  
+    // randomized items
+    function generateRandomItems() {
+      items.forEach(item => {
+        const newItem = document.createElement('img');
+        newItem.classList.add('item');
+        newItem.setAttribute('id', item);
+        newItem.src = 'IMG/CHIP.PNG';
+        newItem.style.position = 'absolute';
+  
+        const x = Math.random() * (window.innerWidth - newItem.width);
+        const y = Math.random() * (window.innerHeight - newItem.height);
+  
+        newItem.style.left = `${x}px`;
+        newItem.style.top = `${y}px`;
+  
+        newItem.addEventListener('click', function () {
+          collectItem(item);
+        });
+  
+        document.body.appendChild(newItem);
+  
+        if (localStorage.getItem(item)) {
+          newItem.style.visibility = 'hidden';
+        }
+      });
+    }
+  
+    // This is the collection system
+    function collectItem(item) {
+      const itemElement = document.getElementById(item);
+      if (itemElement) {
+        itemElement.style.visibility = 'hidden';
+        localStorage.setItem(item, 'collected');
+        collectedItems++;
+        updateCounter();
+      }
+    }
+  
+    // Collection counter
+    function updateCounter() {
+      counterSpan.textContent = collectedItems;
+      localStorage.setItem('collectedItems', collectedItems.toString());
+    }
+  
+    // Flashlight effect
+    const shadow = document.querySelector('.shadow');
+    document.addEventListener('mousemove', function (e) {
+      const x = e.clientX - (document.documentElement.clientWidth * 1.5);
+      const y = e.clientY - (document.documentElement.clientHeight * 1.5);
+      shadow.style.transform = `translate(${x}px, ${y}px)`;
+  
+      items.forEach(item => {
+        const itemElement = document.getElementById(item);
+        if (itemElement) {
+          const rect = itemElement.getBoundingClientRect();
+          const isMouseOver = isMouseOverElement(e, rect, flashlightRadius);
+          itemElement.style.visibility = isMouseOver ? 'visible' : 'hidden';
+  
+          if (isMouseOver && localStorage.getItem(item)) {
+            itemElement.style.visibility = 'hidden';
+          }
+        }
+      });
+    });
+  
+    // If the cursor is hovering over an object it shows
+    function isMouseOverElement(event, rect, radius) {
+      const { clientX, clientY } = event;
+      const centerX = (rect.left + rect.right) / 2;
+      const centerY = (rect.top + rect.bottom) / 2;
+      const distance = Math.sqrt((clientX - centerX) ** 2 + (clientY - centerY) ** 2);
+      return distance <= radius;
+    }
+  
+    // Setup
+    generateRandomItems();
+    updateCounter();
+  });
+  
---
+++ readme.md
@@ -0,0 +1,9 @@
+ITEM COLLECTOR GAME
+
+The way the game works is that you need to find the 3 different items using your flashlight. When you have found them all u need to press them in order for them to get collected, after that you're done! Or you could press the restart button to play again.
+
+1. Hover the cursor over items to reveal them with the flashlight.
+2. Click on the items to collect them.
+3. Click the "Restart" button to reset the game and start collecting items again.
+
+And most importantly have fun playing the Item Collector Game!
